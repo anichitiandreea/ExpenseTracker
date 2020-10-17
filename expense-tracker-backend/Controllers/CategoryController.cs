e2b00ac1bf439e05153baa8330820dc6df66e5b8
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-17 08:58:52 +0300

    Implement CategoryController GetAll and Create endpoints

---
+++ expense-tracker-backend/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using expense_tracker_backend.Domain;
+using expense_tracker_backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace expense_tracker_backend.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [Route("categories")]
+        public async Task<ActionResult> GetAllAsync()
+        {
+            var categories = await categoryService.GetAllAsync();
+
+            if(categories is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        [Route("categories")]
+        public async Task<ActionResult> CreateAsync([FromBody] Category category)
+        {
+            if(category is null)
+            {
+                return BadRequest();
+            }
+
+            await categoryService.CreateAsync(category);
+
+            return StatusCode(201, category);
+        }
+    }
+}
---
+++ expense-tracker-backend/Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using expense_tracker_backend.Database;
+using expense_tracker_backend.Domain;
+using expense_tracker_backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace expense_tracker_backend.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly DatabaseContext context;
+
+        public CategoryService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetByIdAsync(Guid id)
+        {
+            return await context.Categories
+                .FirstOrDefaultAsync(category => category.Id == id
+                && !category.IsDeleted);
+        }
+
+        public async Task CreateAsync(Category category)
+        {
+            await context.Categories.AddAsync(category);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}

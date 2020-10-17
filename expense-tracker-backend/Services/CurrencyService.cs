49ef44dd1a4e1cb72aa37e953b8c90d974da2324
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-17 15:36:36 +0300

    Create CurrencyService

---
+++ expense-tracker-backend/Services/CurrencyService.cs
@@ -0,0 +1,26 @@
+﻿using expense_tracker_backend.Database;
+using expense_tracker_backend.Domain.Domains;
+using expense_tracker_backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace expense_tracker_backend.Services
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly DatabaseContext context;
+
+        public CurrencyService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Currency>> GetAllAsync()
+        {
+            return await context.Currencies.ToListAsync();
+        }
+    }
+}
---
+++ expense-tracker-backend/Services/Interfaces/ICurrencyService.cs
@@ -0,0 +1,11 @@
+﻿using expense_tracker_backend.Domain.Domains;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace expense_tracker_backend.Services.Interfaces
+{
+    public interface ICurrencyService
+    {
+        Task<List<Currency>> GetAllAsync();
+    }
+}

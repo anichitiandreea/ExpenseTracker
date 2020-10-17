f3b208f00c1b8ecef50e4b62da3af424fbc5c5dc
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-17 08:58:17 +0300

    Add CORS configuration

--- expense-tracker-backend/Startup.cs
+++ expense-tracker-backend/Startup.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using expense_tracker_backend.Database;
+using expense_tracker_backend.Services;
+using expense_tracker_backend.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +15,11 @@ namespace expense_tracker_backend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddCors();
+            services.AddDbContext<DatabaseContext>(options => options.UseNpgsql("Server=localhost;Port=5432;Database=ExpenseTracker;User Id=postgres;Password=parola;"));
+
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +32,15 @@ namespace expense_tracker_backend
 
             app.UseRouting();
 
+            app.UseCors(
+                options => options.SetIsOriginAllowed(x => _ = true).AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+            );
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}/{id?}");
             });
         }
     }

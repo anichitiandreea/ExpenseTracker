f81781a80e7a1f714153d705ade95e55836c4b74
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-17 08:54:14 +0300

    Change Currency entity

--- expense-tracker-backend/Domain/Category.cs
+++ expense-tracker-backend/Domain/Category.cs
@@ -1,13 +1,16 @@
-﻿using expense_tracker_backend.Domain.Enums;
+﻿using expense_tracker_backend.Domain.Domains;
+using expense_tracker_backend.Domain.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace expense_tracker_backend.Domain
 {
     public class Category : Entity
     {
-        public Currency Currency { get; set; }
         public string Icon { get; set; }
+        public string IconColor { get; set; }
         public string Name { get; set; }
+        public Guid CurrencyId { get; set; }
         public List<Transaction> Transactions { get; set; }
     }
 }
--- expense-tracker-backend/Domain/Currency.cs
+++ expense-tracker-backend/Domain/Currency.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace expense_tracker_backend.Domain.Enums
+namespace expense_tracker_backend.Domain.Domains
 {
     public class Currency
     {
         public Guid Id { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
+        public List<Category> Categories { get; set; }
     }
 }

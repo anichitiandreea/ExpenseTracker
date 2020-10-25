c277c09ca58c8290b82b58d4dba70d58cd823836
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-10-25 09:09:14 +0200

    Add test project to solution

---
+++ tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using expense_tracker_backend.Controllers;
+using expense_tracker_backend.Domain;
+using expense_tracker_backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Controllers
+{
+    public class AccountControllerTest
+    {
+        private readonly Mock<IAccountService> mockAccountService;
+        private readonly AccountController accountController;
+
+        public AccountControllerTest()
+        {
+            mockAccountService = new Mock<IAccountService>();
+            accountController = new AccountController(mockAccountService.Object);
+        }
+
+        public async Task GivenGetAllAsyncWhenDataExistsThenReturnsData()
+        {
+            // Arrange
+            mockAccountService
+                .Setup(_ => _.GetAllAsync())
+                .ReturnsAsync(new List<Account>())
+                .Verifiable();
+
+            // Act
+            var result = await accountController.GetAllAsync();
+
+            // Assert
+            mockAccountService.VerifyAll();
+            var apiResponse = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(apiResponse.StatusCode, (int)HttpStatusCode.OK);
+        }
+    }
+}

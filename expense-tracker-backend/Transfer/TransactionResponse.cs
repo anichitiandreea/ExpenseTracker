b9bc507a364c90bc5dea7943221d827ef7d01875
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-11-05 23:56:17 +0200

    Create TransactionResponse dto

--- expense-tracker-backend/Transfer/TransactionResponse.cs
+++ expense-tracker-backend/Transfer/TransactionResponse.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace expense_tracker_backend.Transfer
 {
-    public class TransactionRequest
+    public class TransactionResponse
     {
         public TransactionType TransactionType { get; set; }
         public DateTime TransactionDate { get; set; }
-        public string Amount { get; set; }
+        public double Amount { get; set; }
         public string Note { get; set; }
-        public Guid CategoryId { get; set; }
-        public Guid AccountId { get; set; }
+        public string CategoryName { get; set; }
+        public string AccountName { get; set; }
     }
 }

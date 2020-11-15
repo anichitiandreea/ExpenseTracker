30f7864834423a1b7ea16fd0b1cb8bb7283703ee
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-11-15 21:14:43 +0200

    Implement AccountResponse and CategoryResponse transfer models

--- expense-tracker-backend/Transfer/TransactionResponse.cs
+++ expense-tracker-backend/Transfer/TransactionResponse.cs
@@ -5,11 +5,12 @@ namespace expense_tracker_backend.Transfer
 {
     public class TransactionResponse
     {
+        public Guid Id { get; set; }
         public TransactionType TransactionType { get; set; }
         public DateTime TransactionDate { get; set; }
         public double Amount { get; set; }
         public string Note { get; set; }
-        public string CategoryName { get; set; }
-        public string AccountName { get; set; }
+        public CategoryResponse Category { get; set; }
+        public AccountResponse Account { get; set; }
     }
 }
